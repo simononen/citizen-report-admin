b8049a6ce3b3e582e1e5b8ced5a1a94935e78458
Simon Onen <sonen8524@gmail.com>
2021-05-11 13:01:40 +0300

    Chore: Modify the login and remove default ring color

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,19 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../shared/material/material.module';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
--- src/app/dashboard/civic-education/illustrated-stories/new-illustrated-story/new-illustrated-story.component.ts
+++ src/app/dashboard/civic-education/illustrated-stories/new-illustrated-story/new-illustrated-story.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-illustrated-story',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewIllustratedStoryComponent implements OnInit {
 
-  constructor() { }
+  illustratedStoriesForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+   }
 
   ngOnInit(): void {
+    this.setUpIllustratedStoryForm();
+  }
+
+  setUpIllustratedStoryForm(): void {
+    this.illustratedStoriesForm = this.fb.group({
+      'title': ['', Validators.required],
+      'url': ['', Validators.required],
+      'description': [''],
+      'tag': [''],
+      'author': [''],
+      'showcase': [false, '']
+    });
+  }
+
+  onSave(form: FormGroup) {
+    let data = {
+      'title': form.controls.title.value,
+      'url': form.controls.url.value,
+      'description': form.controls.description.value,
+      'tag': form.controls.tag.value,
+      'author': form.controls.author.value,
+      'showcase': form.controls.showcase.value
+
+    }
+    console.log('Form Values ', data);
+  }
+
+  get title() {
+    return this.illustratedStoriesForm.get('title');
+  }
+
+  get url() {
+    return this.illustratedStoriesForm.get('url');
   }
 
 }
