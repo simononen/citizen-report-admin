15fb8d6f45c7adc8cb009464e6b03ec236c872b1
Simon Onen <sonen8524@gmail.com>
2021-05-11 16:54:17 +0300

    Chore: Modify the login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
+
+import { AuthService } from 'src/app/auth/_services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +11,34 @@ import { Route, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private router: Router,
+    private _authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.setUpLoginForm();
   }
 
-  login() {
-    console.log('clicked');
+  setUpLoginForm() {
+    this.loginForm = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', Validators.required]
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  login(form: FormGroup) {
     this.router.navigate(['directorate/districts']);
   }
 
