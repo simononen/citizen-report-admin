3ff5018f303e2b51bd6c7fb415cde3f2c0e42833
Simon Onen <sonen8524@gmail.com>
2021-05-12 16:01:46 +0300

    Chore: Modify the district interface to json api spec

--- src/app/shared/interfaces/district/IDistrict.ts
+++ src/app/shared/interfaces/district/IDistrict.ts
@@ -1,4 +1,13 @@
 export interface IDistrict {
+  data: IDistrict
+}
+
+interface IDistrictData {
+  type?: string,
+  attributes: IDistrictAttributes
+}
+
+interface IDistrictAttributes {
   id?: number | string;
   s_n?: number | string,
   region?: string,
