7423fdeecaec0eec5c34f7b13a4c042a87ecded3
Simon Onen <sonen8524@gmail.com>
2021-05-13 00:53:18 +0300

    Fix: Fix search

--- src/app/dashboard/directorate/district/district-list/district-list.component.ts
+++ src/app/dashboard/directorate/district/district-list/district-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IDistrict, IDistricts, IMeta } from 'src/app/shared/interfaces/district/IDistrict';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -6,6 +7,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { DistrictService } from '../_services/district.service';
 import { ILink } from '../../../../shared/interfaces/district/IDistrict';
 import Swal from 'sweetalert2'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-district-list',
@@ -14,9 +16,13 @@ import Swal from 'sweetalert2'
 })
 export class DistrictListComponent implements OnInit {
 
-  isLoading: Boolean = false;
+  apiUrl = environment.apiUrl
+
+  isLoading!: Boolean;
   errorMessage: string = '';
   districts$!: Observable<IDistrict[] | any>;
+
+  districtSearchForm!: FormGroup;
 
   links!: ILink;
   meta!: IMeta;
@@ -28,18 +34,27 @@ export class DistrictListComponent implements OnInit {
   lastPage!: number;
 
   constructor(
+    private fb: FormBuilder,
     private _districtService: DistrictService,
   ) { }
 
   ngOnInit(): void {
     this.getAllDistricts('');
+    this.setUpDistrictSearchForm();
+  }
+
+  setUpDistrictSearchForm(): void {
+    this.districtSearchForm = this.fb.group({
+      'searchQuery': ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
 
   getAllDistricts(url: string) {
-
+    this.isLoading = true;
     this.districts$ = this._districtService.getAllDistricts(url).pipe(
       tap(
         data => {
+          this.isLoading = false;
           // @ts-ignore
           this.links = data['links'],
           // @ts-ignore
@@ -50,16 +65,26 @@ export class DistrictListComponent implements OnInit {
 
           this.currentPage = this.meta.current_page;
           this.lastPage = this.meta.last_page;
-
-          console.log('Previous ', this.previousPageNavigation);
         }
       ),
       catchError(error => {
+        this.isLoading = false;
         this.errorMessage = error;
         console.log(this.errorMessage);
         return of(null);
       }));
 
+  }
+
+  searchDistricts(query: FormGroup) {
+    let searchQuery = query.controls.searchQuery.value;
+    let searchUrl = `${this.apiUrl}/v1/districts?district=${searchQuery}`;
+    this.getAllDistricts(searchUrl);
+  }
+
+  clearSearch(): void {
+    this.getAllDistricts('');
+    this.searchQuery?.setValue(null);
   }
 
   confirmDelete() {
@@ -85,6 +110,10 @@ export class DistrictListComponent implements OnInit {
         )
       }
     });
+  }
+
+  get searchQuery() {
+    return this.districtSearchForm.get('searchQuery');
   }
 
 
