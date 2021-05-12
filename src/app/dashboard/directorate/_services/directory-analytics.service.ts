b297d71ad68949bb5f08673d9a62b1f04c6f02b9
Simon Onen <sonen8524@gmail.com>
2021-05-12 13:29:59 +0300

    Chore: Add servie for directorate analytics

---
+++ src/app/dashboard/directorate/_services/directory-analytics.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DirectoryAnalyticsService {
+
+  apiUrl = environment.apiUrl
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get() {
+    return this.http.get(this.apiUrl);
+  }
+
+  post() {
+    return;
+  }
+
+  update() {
+    return;
+  }
+
+  show() {
+    return;
+  }
+
+  delete() {
+    return;
+  }
+
+}
