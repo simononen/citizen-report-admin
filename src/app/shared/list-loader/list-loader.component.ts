c3d19ee763d5f553c94b391e6de33d4668d5794b
Simon Onen <sonen8524@gmail.com>
2021-05-12 21:32:24 +0300

    Chore: Add skeleton loader

---
+++ src/app/shared/list-loader/list-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-loader',
+  templateUrl: './list-loader.component.html',
+  styleUrls: ['./list-loader.component.scss']
+})
+export class ListLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { DashboardLoaderComponent } from './dashboard-loader/dashboard-loader.component';
+import { ListLoaderComponent } from './list-loader/list-loader.component';
 import { NgModule } from '@angular/core';
 import { NgxContentLoadingModule } from 'ngx-content-loading';
 
 @NgModule({
   declarations: [
-    DashboardLoaderComponent
+    DashboardLoaderComponent,
+    ListLoaderComponent
   ],
   imports: [
     CommonModule,
     NgxContentLoadingModule
   ],
   exports: [
-    DashboardLoaderComponent
+    DashboardLoaderComponent,
+    ListLoaderComponent
   ]
 })
 export class SharedModule { }
